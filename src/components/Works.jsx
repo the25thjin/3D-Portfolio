a83cf2e4c8ac657984f9dfdaf6f3e3f699d147e1
the25thjin <mannr075@gmail.com>
2024-07-07 17:26:36 +0530

    adding all files

---
+++ src/components/Works.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { Tilt } from "react-tilt";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt="project_image"
+          className="w-full h-full object-contain"
+        />
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer" onClick={()=>window.open(source_code_link,"_blank")} >
+            <img className="w-1/2 h-1/2 object-contain" src={github}  alt="source code" />
+          </div>
+
+        </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="text-secondary mt-2 text-[14px]">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag)=>(<p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My Works</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, autem
+          mollitia! Facere, tenetur quasi impedit repellendus ea rerum natus
+          quam officiis accusantium! Corporis dolor rerum assumenda fugiat quam.
+          Voluptates, ipsam?
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
